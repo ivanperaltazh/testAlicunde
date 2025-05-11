a6dc8f0de28aec03ad3042918185f786aae04b00
Iván <ivanperaltaz30@gmail.com>
2025-05-11 18:28:48 +0200

    se añade contenido

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -4,7 +4,15 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   imports: [ ],
-  template: `<p>Home wokrs</p>`,
+  template: `
+  <h3>Contenido</h3>
+  <ul>
+    <li>Spinner global en Interceptores</li>
+    <li>Store estado global simple con signals y servicio</li>
+    <li>Arquitectura Scalfolding-documentacion</li>
+    <li>Arrancar "json-serve" abrir nuevo terminal y ejecutar > npm run serve:api</li>
+  </ul>
+  `,
   styleUrl: './home.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
